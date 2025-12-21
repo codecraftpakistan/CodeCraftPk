cc1a5030f974ecd9ce2098de198e179b584d4048
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-12-21 22:43:29 +0500

    Update mailingvariables.php

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,8 @@
 <?php
-    $mail_host = "smtp.gmail.com";
-    $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
-    $mail_sender_name = "Website Form";
+// Gmail SMTP settings
+$mail_host            = "smtp.gmail.com";
+$mail_port            = "587"; // TLS. If you prefer SSL, use 465 and ENCRYPTION_SMTPS.
+$mail_sender_email    = "codecraftpakistan@gmail.com";      // <-- Gmail address you own
+$mail_sender_password = "xdat ilqf tshk fdcd";     // <-- 16-char App Password from Google
+$mail_sender_name     = "Website Form";
 ?>
